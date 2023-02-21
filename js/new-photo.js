d2487c1d2a72f27e6b314967594da6535ca8e2fd
Stepan <ose.26@mail.ru>
2023-02-21 20:52:10 +0500

    feature(new.js): add new photo form and listeners to close it

---
+++ js/new-photo.js
@@ -0,0 +1,47 @@
+import {isEscapeKey} from './utils.js';
+
+const uploadPhotoInput = document.querySelector('#upload-file');
+const uploadPhotoOverlay = document.querySelector('.img-upload__overlay');
+const uploadCancelBtn = document.querySelector('#upload-cancel');
+
+const onUploadPhotoInputChange = (evt) => {
+  evt.preventDefault();
+  if (uploadPhotoInput.value) {
+    openUploadOverlay();
+  }
+};
+
+const onUploadPhotoEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUploadOverlay();
+  }
+};
+
+const onUploadCancelBtnClick = (evt) => {
+  evt.preventDefault();
+  closeUploadOverlay();
+};
+
+function openUploadOverlay() {
+  uploadPhotoOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onUploadPhotoEscKeydown);
+  uploadCancelBtn.addEventListener('click', onUploadCancelBtnClick);
+}
+
+function closeUploadOverlay() {
+  uploadPhotoOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onUploadPhotoEscKeydown);
+  uploadCancelBtn.removeEventListener('click', onUploadCancelBtnClick);
+
+  uploadPhotoInput.value = null;
+}
+
+
+uploadPhotoInput.addEventListener('change', onUploadPhotoInputChange);
+
+export {openUploadOverlay, closeUploadOverlay};
