f4953ea9876027e5529fef2b6925f420722dc8ac
Stepan <ose.26@mail.ru>
2023-02-27 23:22:09 +0500

    refactoring(error.js, success.js): correct removes listeners

--- js/error.js
+++ js/error.js
@@ -4,33 +4,43 @@ const errorTemplate = document.querySelector('#error')
   .content
   .querySelector('.error');
 
-const closeError = () => {
-  document.querySelector('.error').remove();
+const onCloseErrorClick = (evt) => {
+  evt.preventDefault();
+  closeError();
 };
+
+const onErrorClick = (evt) => {
+  if (evt.target.nodeName !== 'DIV') {
+    evt.preventDefault();
+    closeError();
+  }
+};
+
+const  onEscKeyDown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeError();
+  }
+};
+
+function closeError() {
+  const errorElement = document.querySelector('.error');
+
+  errorElement.querySelector('.error__button').removeEventListener('click', onCloseErrorClick);
+  errorElement.removeEventListener('click', onErrorClick);
+  document.removeEventListener('keydown', onEscKeyDown);
+
+  errorElement.remove();
+}
 
 const showError = (errorText) => {
   const errorFragment = document.createDocumentFragment();
   const errorElement = errorTemplate.cloneNode(true);
   errorElement.querySelector('.error__title').textContent = errorText;
 
-  errorElement.querySelector('.error__button').addEventListener('click', (evt) => {
-    evt.preventDefault();
-    closeError();
-  }, {once: true});
-
-  errorElement.addEventListener('click', (evt) => {
-    if (evt.target.nodeName !== 'DIV') {
-      evt.preventDefault();
-      closeError();
-    }
-  }, {once: true});
-
-  document.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      evt.preventDefault();
-      closeError();
-    }
-  }, {once: true});
+  errorElement.querySelector('.error__button').addEventListener('click', onCloseErrorClick);
+  errorElement.addEventListener('click', onErrorClick);
+  document.addEventListener('keydown', onEscKeyDown);
 
   errorFragment.appendChild(errorElement);
   document.body.appendChild(errorFragment);
--- js/success.js
+++ js/success.js
@@ -4,33 +4,43 @@ const successTemplate = document.querySelector('#success')
   .content
   .querySelector('.success');
 
-const closeSuccess = () => {
-  document.querySelector('.success').remove();
+const onCloseSuccessClick = (evt) => {
+  evt.preventDefault();
+  closeSuccess();
 };
+
+const onSuccessClick = (evt) => {
+  if (evt.target.nodeName !== 'DIV') {
+    evt.preventDefault();
+    closeSuccess();
+  }
+};
+
+const  onEscKeyDown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeSuccess();
+  }
+};
+
+function closeSuccess()  {
+  const successElement = document.querySelector('.success');
+
+  successElement.querySelector('.success__button').removeEventListener('click', onCloseSuccessClick);
+  successElement.removeEventListener('click', onSuccessClick);
+  document.removeEventListener('keydown', onEscKeyDown);
+
+  successElement.remove();
+}
 
 const showSuccess = (successText) => {
   const successFragment = document.createDocumentFragment();
   const successElement = successTemplate.cloneNode(true);
   successElement.querySelector('.success__title').textContent = successText;
 
-  successElement.querySelector('.success__button').addEventListener('click', (evt) => {
-    evt.preventDefault();
-    closeSuccess();
-  }, {once: true});
-
-  successElement.addEventListener('click', (evt) => {
-    if (evt.target.nodeName !== 'DIV') {
-      evt.preventDefault();
-      closeSuccess();
-    }
-  }, {once: true});
-
-  document.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      evt.preventDefault();
-      closeSuccess();
-    }
-  }, {once: true});
+  successElement.querySelector('.success__button').addEventListener('click', onCloseSuccessClick);
+  successElement.addEventListener('click', onSuccessClick);
+  document.addEventListener('keydown', onEscKeyDown);
 
   successFragment.appendChild(successElement);
   document.body.appendChild(successFragment);
